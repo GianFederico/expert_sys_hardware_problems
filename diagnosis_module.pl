1787cb57707eba58b4a347795d36a6c6755c9133
GianFederico <48125720+GianFederico@users.noreply.github.com>
2023-06-28 20:30:10 +0200

    implemented why and how questions, minor changes

--- diagnosis_module.pl
+++ diagnosis_module.pl
@@ -1,83 +1,47 @@
-:- module(diagnosis_module, [diagnosis/1, questioncode/2]).
+:- module(diagnosis_module, [diagnosis/1, questioncode/3]).
 :- use_module(user_interaction_module).
-%:- dynamic(diagnosis/1).
+:- dynamic(diagnosis/1).
 
 /* Top-level diagnosis rules */
 diagnosis('PSU Turned OFF.') :- power_problem_current, askifnot(psu_on).
-diagnosis('PSU not plugged in.') :- power_problem_current, askifnot(psu_on), askifnot(just_plugged).
+diagnosis('PSU not plugged in.') :- power_problem_current, askif(psu_on), askifnot(just_plugged).
 diagnosis('Insufficient PSU wattage.') :- askif(pc_dead), psu_problem, askif(crashes), askif(more_info), askfor(power_supply), askfor(cpu), askfor(gpu), askfor(mobo),
                                             cpu(_,Cpu_wattage), gpu(Gpu_wattage), power_supply(Psu_wattage), mobo(_,_,Tdp), 
                                             Required_wattage is Cpu_wattage + Gpu_wattage + Tdp + 50,
                                             Psu_wattage < Required_wattage.
 diagnosis('Incompatible RAM.') :- askifnot(pc_dead), askif(lights_turn_on), askif(more_info), askfor(mobo), askfor(ram), 
-                                    ram(Type), mobo(_,Ram_comp,_), dif(Type, Ram_comp).  %The dif/2 predicate is a constraint that is true if and only if A and B are different terms. If A and B can never unify, dif/2 succeeds deterministically. If A and B are identical, it fails immediately.
+                                    ram(Type), mobo(_,Ram_comp,_), dif(Type, Ram_comp).  %The dif/2 predicate is a constraint that is true if and only if A and B are different terms. 
+                                                                                         %If A and B can never unify, dif/2 succeeds deterministically. If A and B are identical, it fails immediately.
 diagnosis('Incompatible CPU.') :- askifnot(pc_dead), askifnot(lights_turn_on), askif(more_info), askfor(mobo), askfor(cpu), 
                                     cpu(Manifacturer,_), mobo(Cpu_comp,_,_), dif(Manifacturer, Cpu_comp).
-diagnosis('CPU thermal throttle, insufficient cooling.') :- slow_performance, cpu_problem.
-diagnosis('Old HDD, You need to replace it.') :- slow_performance, mechanical_problem_hdd, askif(more_info), askfor(hdd), hdd(Hdd_age), Hdd_age>4.
-diagnosis('GPU thermal throttle, insufficient cooling.') :- slow_performance, gpu_problem.
+diagnosis('CPU thermal throttle, insufficient cooling.') :- askif(pc_dead), slow_performance, cpu_problem.
+diagnosis('GPU thermal throttle, insufficient cooling.') :- askif(pc_dead), slow_performance, gpu_problem.
+diagnosis('Old HDD, You need to replace it.') :- askif(pc_dead), slow_performance, mechanical_problem_hdd, askif(more_info), askfor(hdd), hdd(Hdd_age), Hdd_age>4.
 diagnosis('Damaged HDD, may require replacement.') :- askif(pc_dead), mechanical_problem_hdd.
 diagnosis('Debris or cables between fan blades.') :- askif(pc_dead), mechanical_problem_fans.
-diagnosis('Some cables are unplugged, check every internal cable.') :- power_problem_current, askifnot(psu_on), askif(just_plugged).
+diagnosis('Some cables are unplugged, check every internal cable.') :- power_problem_current, askif(psu_on), askif(just_plugged).
 diagnosis('Out-of-date BIOS.') :- askif(new_component).
-diagnosis('PSU may be underspec.') :- askif(pc_dead), psu_problem, askif(crashes), askif(more_info), askfor(power_supply), askfor(cpu), askfor(gpu), askfor(mobo),
-                                            cpu(_,Cpu_wattage), gpu(Gpu_wattage), power_supply(Psu_wattage), mobo(_,_,Tdp), 
-                                            Required_wattage is Cpu_wattage + Gpu_wattage + Tdp,
-                                            Psu_wattage < Required_wattage.
-diagnosis('I am sorry. I cannot find the problem with the information you provided.').
-
+diagnosis('I am sorry. I cannot find any problem with the information you provided.').
 
 /* Definitions of intermediate predicates */                            
 power_problem_current :- askifnot(pc_dead).                             
-
 mechanical_problem_hdd :- askif(has('an HDD')), askif(hear('whiny noise from the HDD')).
 mechanical_problem_fans :- askif(hear('weird noise from the fans')).
-
 slow_performance :- askif(slow_pc).
-cpu_problem :- askif(cpu_termal).
-gpu_problem :- askif(gpu_termal).
+cpu_problem :- askif(cpu_thermal).
+gpu_problem :- askif(gpu_thermal).
 psu_problem :- askif(crashes).
 
-specific_problem :- askif(more_info), askfor(power_supply), askfor(cpu), askfor(gpu), askfor(ram), askfor(hdd), askfor(ssd), askfor(mobo).
-
 /* Question decoding */
-questioncode(more_info, 'Are you able to give me some more specific information').
-questioncode(pc_dead, 'Does the pc turn on').
-questioncode(cpu_termal, 'Does the CPU run over 85C').
-questioncode(gpu_termal, 'Does the GPU run over 85C').
-questioncode(slow_pc, 'Does the PC run slow').
-questioncode(handyperson, 'Are you confident in fixing things').
-questioncode(psu_on, 'Is the ON/OFF switch of the PSU set to ON').
-questioncode(just_plugged, 'Did you plug the PC in').
-questioncode(has(X), X) :- write('Does the PC have ').
-questioncode(hear(X), X) :- write('Do you hear a ').
-questioncode(lights_turn_on, 'Do the lights turn on even for a second').
-questioncode(crashes, 'Does the PC crashes often while under load').
-questioncode(new_component, 'Do you have recently installed a new component').
-
-
-
-
-
-
-
-
-
-
-
-
-% % rules
-
-% compatible(X, Y) :- X = psu, power_supply(Y, W), W >= 500.
-% compatible(X, Y) :- X = cpu, cpu(Y, intel, _, _, _).
-% compatible(X, Y) :- X = graphics_card, graphics_card(Y, _, _, _, _, _).
-% compatible(X, Y) :- X = motherboard, motherboard(Y, intel, _, _, _, _, _, _, _).
-% compatible(X, Y) :- X = ram, ram(Y, ddr4, _, _).
-% compatible(X, Y) :- X = hard_drive, hard_drive(Y, _, _, sata3).
-% compatible(X, Y) :- X = solid_state_drive, solid_state_drive(Y, _, sata3, _).
-% compatible(X, Y) :- X = cpu_cooler, cpu_cooler(Y, air, _).
-% compatible(X, Y) :- X = gpu_cooler, gpu_cooler(Y, fan).
-
-% requires(X, Y) :- X = cpu, Y = psu
-
-
+questioncode(more_info, 'Are you able to provide some more specific info about components','  your problem could be related to some specific features of your components').
+questioncode(pc_dead, 'Does the pc turn on','   this is the most basic question in order to discriminate between problems').
+questioncode(cpu_thermal, 'Does the CPU run near or more than 85C','  sustained cpu temps near or more than 85C are not good, best if under 79C').
+questioncode(gpu_thermal, 'Does the GPU run near or more than 85C','  sustained gpu temps near or more than 85C are not good, best if under 79C').
+questioncode(slow_pc, 'Does the PC run slow','  if it runs slow there likely is a problem somewhere').
+questioncode(psu_on, 'Is the ON/OFF switch of the PSU set to ON','  maybe you just forgot to turn the PSU on').
+questioncode(just_plugged, 'Did you plug the PC in','   maybe you just forgot to plug it in').
+questioncode(has(X), X,'I need to know if you have this component to include it in the diagnosis') :- write('Does the PC have ').
+questioncode(hear(X), X,'I need to knwo if you hear this kind of noise to discriminate between problems') :- write('Do you hear a ').
+questioncode(lights_turn_on, 'Do the lights turn on, even for a second','    to understand if current flows in your sys').
+questioncode(crashes, 'Does the PC crashes often while under load','    to understand if the PC creshes when it draws more power').
+questioncode(new_component, 'Do you have recently installed a new component','  new components generally need some software update').
--- user_interaction_module.pl
+++ user_interaction_module.pl
@@ -1,84 +1,103 @@
 :- module(user_interaction_module, [askif/1, askifnot/1, start/0, askfor/1]).
-
 :- dynamic(asked/2).
+:- dynamic user_answers/1.
 
-askif(Q) :- ask(Q,A), positive_answer(Q,A).
+accepted_answer(_,A) :- affirmative(A).
+accepted_answer(Qcode,A) :- \+(negative(A)), \+(affirmative(A)), \+(interrogative(A)),
+                            write('   invalid answer: reply with y, n or w.'), 
+                            read(A2), retract(asked(Qcode,A)), assertz(asked(Qcode,A2)), affirmative(A2).
 
-positive_answer(_,A) :- affirmative(A).
-positive_answer(Qcode,A) :- \+(negative(A)), \+(affirmative(A)), write('Please answer yes or no.'), read(A2), retract(asked(Qcode,A)), asserta(asked(Qcode,A2)), affirmative(A2).
-
-askifnot(Q) :- not(askif(Q)).
+interrogative_answer(Qcode,A) :- interrogative(A),
+                            questioncode(Qcode, _, E),
+                            write(E), read(A2), retract(asked(Qcode,A)), assertz(asked(Qcode,A2)), affirmative(A2).
 
 ask(Qcode,A) :- asked(Qcode,A).
-ask(Qcode,A) :- \+(asked(Qcode,A)), questioncode(Qcode,Q), write(Q), write('?'), read(A2), ask2(Q,Qcode,A2,A).
+ask(Qcode,A) :- \+(asked(Qcode,A)), 
+                questioncode(Qcode,Q,_), write(Q), write('? '), read(A2), ask2(Q,Qcode,A2,A).
 
-ask2(_,Qcode,'?',A) :- explain(Qcode), ask(Qcode,A).
-ask2(_,Qcode,A,A) :- \+(A = '?'), asserta(asked(Qcode,A)).
+ask2(_,Qcode,A,A) :-  assertz(asked(Qcode,A)).
+
+askif(Q) :- ask(Q,A), accepted_answer(Q,A).
+askif(Q) :- ask(Q,A), interrogative_answer(Q,A).
+askifnot(Q) :- not(askif(Q)).
+
+askinfo(Question, Answer) :- write(Question), read(Answer).
 
 affirmative(yes).
 affirmative(y).
-affirmative(ye).
-affirmative(right).
+affirmative(yup).
 affirmative(ok).
-affirmative(uhhuh).
 
 negative(no).
 negative(n).
 negative(not).
 negative(never).
-negative(impossible).
-negative(haha).
+negative(nope).
 
+interrogative(w).
+interrogative(why).
 
-
-
+printfacts :- nl, write('   This is how I got the diagnosis:'), nl, forall(asked(Q, A), print_fact(Q, A)).
+print_fact(Q, A) :- format('        To the question: ~w,     you replied: ~w~n', [Q, A]).
 
 reset :-
-    retractall(known(_, _, _)). %da rivedere@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    retractall(asked(_, _)),
+    retractall(diagnosis_module:power_supply(_)),
+    retractall(diagnosis_module:cpu(_,_)),
+    retractall(diagnosis_module:gpu(_)),
+    retractall(diagnosis_module:ram(_)),
+    retractall(diagnosis_module:mobo(_,_,_)),
+    retractall(diagnosis_module:diagnosis(_)).
 
 start :-
-    reset, nl,
-    write('Welcome to FAULT-FINDER!'), nl,
-    write('let me ask some question about your pc-related problem:'), nl, nl,
-    diagnosis_module:diagnosis(Diagnosis),
+    consult(diagnosis_module),
     nl,
-    format('DIAGNOSIS: ~w~n', [Diagnosis]).
-
-
-askinfo(Question, Answer) :-
-    write(Question),
-    read(Answer).
+    write('Welcome to FAULT-FINDER!'), nl,
+    write('Let me ask some questions about your pc-related problem:'), nl, nl,
+    diagnosis_module:diagnosis(Diagnosis),
+    nl, nl,
+    format('DIAGNOSIS: ~w~n', [Diagnosis]),
+    printfacts,
+    reset.
 
 askfor(power_supply) :-
-    askinfo('   What is the wattage of the power supply? ', Wattage),
-    assertz(diagnosis_module:power_supply(Wattage)).
+    askinfo('   What is the wattage of the power supply (watts)? ', Wattage),
+    assertz(diagnosis_module:power_supply(Wattage)),
+    assertz(asked('psu_wattage',Wattage)).
 
 askfor(cpu) :-
-    askinfo('   What is the manifacturer of the CPU (Intel or AMD)? ', Manifacturer),
-    askinfo('   What is the TDP of the CPU? ', Tdp),
-    assertz(diagnosis_module:cpu(Manifacturer, Tdp)).
+    askinfo('   What is the manifacturer of the CPU (intel or amd)? ', Manifacturer),
+    askinfo('   What is the TDP of the CPU (watts)? ', Tdp),
+    assertz(diagnosis_module:cpu(Manifacturer, Tdp)),
+    assertz(asked('cpu_manifacturer', Manifacturer)),
+    assertz(asked('cpu_tpd',Tdp)).
 
 askfor(gpu) :-
-    askinfo('   What is the TDP of the graphics card? ', Tdp),
-    assertz(diagnosis_module:gpu(Tdp)).
+    askinfo('   What is the TDP of the graphics card (watts)? ', Tdp),
+    assertz(diagnosis_module:gpu(Tdp)),
+    assertz(asked('gpu_tdp',Tdp)).
 
 askfor(ram) :-
     %askinfo('What is the capacity of RAM (in GB)? ', Capacity),
     % askinfo('What is the speed of RAM? ', Speed),
     askinfo('   What is the type of RAM? (ddr?) ', Type),
-    assertz(diagnosis_module:ram(Type)). %ram(Type, Capacity, Speed)
+    assertz(diagnosis_module:ram(Type)), %ram(Type, Capacity, Speed)
+    assertz(asked('ram_type',Type)).
 
 askfor(hdd) :-
-    askinfo('   How old is the hard drive?', Age),
-    assertz(diagnosis_module:hdd(Age)).
+    askinfo('   How old is the hard drive? (years)', Age),
+    assertz(diagnosis_module:hdd(Age)),
+    assertz(asked('hdd_age',Age)).
 
 % askfor(ssd) :-
 %     askinfo('How old is the solid state drive?', Age),
 %     assertz(diagnosis_module:ssd(Age)).
 
 askfor(mobo) :-
-    askinfo('   What cpus can the motherboard run (Intel or AMD)', Cpu_comp),
+    askinfo('   What cpus can the motherboard run (intel or amd)', Cpu_comp),
     askinfo('   What type of RAM can the motherboard read (ddr?)', Ram_comp),
-    askinfo('   What is the TDP of the motherboard?', Tdp),
-    assertz(diagnosis_module:mobo(Cpu_comp, Ram_comp, Tdp)).
-
+    askinfo('   What is the TDP of the motherboard (watts)?', Tdp),
+    assertz(diagnosis_module:mobo(Cpu_comp, Ram_comp, Tdp)),
+    assertz(asked('mobo_cpu_comp',Cpu_comp)),
+    assertz(asked('mobo_ram_comp',Ram_comp)),
+    assertz(asked('mobo_tdp',Tdp)).
